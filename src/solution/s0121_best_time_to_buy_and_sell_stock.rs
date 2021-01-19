41e5f22cd81cbd15fc06e0d7698ba6906dd0a72d
LeeReindeer <LeeReix@live.com>
2021-01-19 23:41:23 +0800

    Update 121

--- src/solution/s0121_best_time_to_buy_and_sell_stock.rs
+++ src/solution/s0121_best_time_to_buy_and_sell_stock.rs
@@ -24,6 +24,19 @@ impl Solution {
         }
         max
     }
+
+    pub fn max_profit_brute(prices: Vec<i32>) -> i32 {
+        let mut max = 0;
+        for i in 0..prices.len() {
+            for j in i + 1..prices.len() {
+                let profit = prices[j] - prices[i];
+                if profit > max {
+                    max = profit;
+                }
+            }
+        }
+        max
+    }
 }
 
 // submission codes end
@@ -37,5 +50,9 @@ mod tests {
         assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
         assert_eq!(Solution::max_profit(vec![7, 6, 4, 3, 1]), 0);
         assert_eq!(Solution::max_profit(vec![]), 0);
+
+        assert_eq!(Solution::max_profit_brute(vec![7, 1, 5, 3, 6, 4]), 5);
+        assert_eq!(Solution::max_profit_brute(vec![7, 6, 4, 3, 1]), 0);
+        assert_eq!(Solution::max_profit_brute(vec![]), 0)
     }
 }
